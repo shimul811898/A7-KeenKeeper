0e02cd68e6ea2b0c3f3e0df69af1ca59713fed01
shimul811898 <shimulislam01799802008@gmail.com>
2026-05-06 00:48:35 +0600

    Banner designing

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Banner from "../../components/shared/homepage/Banner";
 
 const Homepage = () => {
     return (
         <div>
-            Homepage
+            <Banner/>
         </div>
     )
 
